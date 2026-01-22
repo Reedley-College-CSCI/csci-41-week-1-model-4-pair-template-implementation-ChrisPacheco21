70e23ecf9f09f41e668d2fc03311fdabe3ea25a7
Christopher Pacheco Hernandez <1002047@MY.SCCCD.EDU>
2026-01-22 14:20:42 -0800

    Added test cases in main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-// Your Names
+// Christopher Pacheco
 #include "Pair.h"
 #include <iostream>
 using namespace std;
 
 int main() {
     // Add test cases here
+    Pair<int>intConstructor(1, 2);
+    Pair<string>stringConstructor("apple", "banana");
+
     return 0;
 }
